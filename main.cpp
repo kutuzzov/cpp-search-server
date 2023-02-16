fe2d392e30b791ffda1f3bc53992cf76f481c048
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2023-02-16 08:37:37 +0300

    Update final version

--- main.cpp
+++ main.cpp
@@ -1,27 +1,16 @@
-#include "search_server.h"
-#include "read_input_functions.h"
-#include "request_queue.h"
-#include "paginator.h"
-#include "document.h"
-#include "string_processing.h"
-#include "test_example_functions.h"
 #include "process_queries.h"
+#include "search_server.h"
+#include "test_example_functions.h"
 
-#include "log_duration.h"
+#include <execution>
+#include <iostream>
+#include <string>
+#include <vector>
 
-using namespace std::string_literals;
 using namespace std;
-
-/*void PrintDocument(const Document& document) {
-    cout << "{ "s
-         << "document_id = "s << document.id << ", "s
-         << "relevance = "s << document.relevance << ", "s
-         << "rating = "s << document.rating << " }"s << endl;
-}*/
 
 int main() {
     SearchServer search_server("and with"s);
-
     int id = 0;
     for (
         const string& text : {
@@ -33,8 +22,6 @@ int main() {
     ) {
         search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
     }
-
-
     cout << "ACTUAL by default:"s << endl;
     // последовательная версия
     for (const Document& document : search_server.FindTopDocuments("curly nasty cat"s)) {
@@ -45,12 +32,10 @@ int main() {
     for (const Document& document : search_server.FindTopDocuments(execution::seq, "curly nasty cat"s, DocumentStatus::BANNED)) {
         PrintDocument(document);
     }
-
     cout << "Even ids:"s << endl;
     // параллельная версия
     for (const Document& document : search_server.FindTopDocuments(execution::par, "curly nasty cat"s, [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 0; })) {
         PrintDocument(document);
     }
-
     return 0;
 }
